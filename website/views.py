89c0eeb2e3f5dfaa30a366fea7c2706d432e0312
Alexandra Li <97430673+shinrannli@users.noreply.github.com>
2022-07-24 23:07:09 -0400

    finish sign-up

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 # whenever you hit this route, the following functions will run
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")

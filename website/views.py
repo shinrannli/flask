a1af5e7f9935c142d32d100b4117cb5099383a75
Alexandra Li <97430673+shinrannli@users.noreply.github.com>
2022-07-25 17:19:21 -0400

    notes remaining

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
 
 # whenever you hit this route, the following functions will run
 @views.route('/')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html",user=current_user)
